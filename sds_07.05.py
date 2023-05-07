3b0e94bb35a4904e833e7170534a64636200a7dd
Comp <nikitamendukov@gmail.com>
2023-05-07 20:14:32 +0300

    домашня 07.05

--- sds_07.05.py
+++ sds_07.05.py
@@ -1,16 +1,16 @@
 #1
-# class People:
-#     def __init__(self, name, age):
-#         self.name = name
-#         self.age = age
-#     def is_adult(self):
-#         if self.age > 18:
-#             return True
-#         else:
-#             return False
-# people = People(input('Name: '), int(input('Age: ')))
-# print(people.is_adult())
-# print(f"Ім'я: {people.name}, Вік: {people.age}")
+class People:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def is_adult(self):
+        if self.age > 18:
+            return True
+        else:
+            return False
+people = People(input('Name: '), int(input('Age: ')))
+print(people.is_adult())
+print(f"Ім'я: {people.name}, Вік: {people.age}")
 
 #2
 class Rectangle:
@@ -25,3 +25,52 @@ class Rectangle:
         return s
 r = Rectangle(int(input('Ширина: ')), int(input('Висота: ')))
 print(f'Периметр = {r.P()}(см), Площа = {r.S()}(см)^2')
+
+#3
+class Car:
+    def __init__(self, brand, model, year):
+        self.brand = brand
+        self.model = model
+        self.year = year
+    def result(self):
+        print(f'Марка: {self.brand}, Модель: {self.model}, Рік: {self.year}')
+car = Car('Audi', 'RS7', 2023 )
+car.result()
+
+#4
+class Bank:
+    def __init__(self):
+        self.customers = []
+    def add_customer(self, customer):
+        self.customers.append(customer)
+        return self.customers
+    def get_total_balance(self):
+        total_balance = 0
+        for customer in self.customers:
+            total_balance += customer.get_balance()
+        return total_balance
+    print('Accounts :')
+class BankAccount:
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
+        print(self.name,':', self.balance)
+    def get_balance(self):
+        return self.balance
+bank = Bank()
+acc1 = BankAccount('Dima', 1000)
+acc2 = BankAccount('Masha', 2000)
+acc3 = BankAccount('Oleg', 1500)
+
+bank.add_customer(acc1)
+bank.add_customer(acc2)
+bank.add_customer(acc3)
+
+print('Total balance:',bank.get_total_balance())
+
+
+
+
+
+
+
